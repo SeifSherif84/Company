cf64509203b29b91472a63ccc60bcff87ac6ab3d
Seif Sherif <seif27168@gmail.com>
2025-09-19 06:49:39 +0300

    Implementation Department DAL

---
+++ Company.DAL/Data/DBContexts/CompanyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Company.DAL.Data.Configurations;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.DBContexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+
+        public CompanyDbContext() : base() 
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-C167I9J; Database = Company; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new DepartmentConfigurations());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
