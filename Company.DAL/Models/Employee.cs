4e5549f99840a03f1a82241888e3ae549c475c8d
Seif Sherif <seif27168@gmail.com>
2025-09-26 20:09:56 +0300

    Add Modifications

--- Company.DAL/Models/Employee.cs
+++ Company.DAL/Models/Employee.cs
@@ -11,33 +11,15 @@ namespace Company.DAL.Models
     public class Employee : BaseEntity
     {
         //public int Id { get; set; }
-
-        [Required(ErrorMessage = "Name Is Required !")]
         public string Name { get; set; }
-
-        [Range(20,60,ErrorMessage = "Age Is Not Valid !")]
         public int? Age { get; set; }
-
-        [DataType(DataType.EmailAddress,ErrorMessage = "Email Is Not Valid !")]
         public string Email { get; set; }
-
-        [RegularExpression(pattern: @"[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$"
-                          ,ErrorMessage = "Address Is Must Like 123-Street,City-Country !")]
         public string Address { get; set; }
-
-        //[DataType(DataType.PhoneNumber)]
-        [Phone]
         public string Phone { get; set; }
-
-        [DataType(DataType.Currency)]
         public decimal Salary { get; set; }
         public bool IsActive { get; set; }
         public bool IsDeleted { get; set; }
-
-        [DisplayName("Hiring Date")]
         public DateTime HiringDate { get; set; }
-
-        [DisplayName("Date Of Creation")]
         public DateTime CreateAt { get; set; }
 
     }
